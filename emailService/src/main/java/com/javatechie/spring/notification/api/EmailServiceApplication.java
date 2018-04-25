7dffc126608c2ac0dd3ab0a7c94a56ad113943f8
Basant kumar Hota <basahota@users.noreply.github.com>
2018-04-25 21:48:08 +0530

    #code

---
+++ emailService/src/main/java/com/javatechie/spring/notification/api/EmailServiceApplication.java
@@ -0,0 +1,22 @@
+package com.javatechie.spring.notification.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/emailService")
+public class EmailServiceApplication {
+
+	@GetMapping("/send")
+	public String sendEmail() {
+		return "Email service called....";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailServiceApplication.class, args);
+	}
+}
---
+++ paytm/src/main/java/com/javatechie/spring/paytm/api/PaytmApplication.java
@@ -0,0 +1,21 @@
+package com.javatechie.spring.paytm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/paymentService")
+public class PaytmApplication {
+	@GetMapping("/pay")
+	public String paymentProcess() {
+		return "Payment service called....";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaytmApplication.class, args);
+	}
+}
