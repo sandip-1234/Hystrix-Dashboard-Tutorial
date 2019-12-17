e76508735b130868b073d95fc79f5eeed52ae812
sandip sharma <sandip.sharmainfogain@gmail.com>
2019-12-17 15:52:28 +0530

    Initial version of hystrix example

---
+++ shopping/src/main/java/com/java/sm/ShoppingApplication.java
@@ -0,0 +1,17 @@
+package com.java.sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
+public class ShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingApplication.class, args);
+	}
+
+}
